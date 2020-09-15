27b8b8c95354306227b815c65b000f9902a5f7c0
Yoshitake Kitamura <kitamura@epea.co.jp>
2020-09-15 02:37:23 +0100

    自動on/offイメージ追加

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const plug = require("./plug/HS105plug.js");
-
 const sensor = require("./sensor/HCSR04.js");
+const awaitDelay = require("./util/awaitDelay.js");
 
-plug.off();
+async function doCheck() {
+  while (true) {
+    await awaitDelay(10000);
+    const distance = sensor.scan();
+    distance > 100 ? plug.off() : plug.on();
+    console.log(distance);
+  }
+}
 
-console.log("[" + sensor.scan() + "]");
+doCheck();
